cb524634db2f5481864cc0d3c690d4c28748500b
Hai Vo <haivotrung@gmail.com>
2022-03-24 11:46:31 -0400

    initial commit

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -1,6 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import LoginForm from '.';
 
+
+
+const badFormInput = {
+  email: "bademail",
+  password: "Hi"
+}
+
 test('renders sign in page', () => {
   render(<LoginForm />);
   const signInText = screen.getByText("Sign in");
@@ -8,3 +15,25 @@ test('renders sign in page', () => {
 });
 
 // Add more unit test here
+
+test('renders 2 input component and submit', () => {
+  render(<LoginForm />);
+  const emailTest = screen.getByText("Email Address");
+  expect(emailTest).toBeInTheDocument();
+
+  const passwordTest = screen.getByText("Password");
+  expect(passwordTest).toBeInTheDocument();
+
+  const submitButton = screen.getByText("Sign In")
+  expect(submitButton).toBeInTheDocument();
+});
+
+test('good validation', () => {
+  const goodFormInput = {
+  email: "hai@ediciones.nyc",
+  password: "ThisIsGre@t14"
+  };
+  
+  render(<LoginForm />);
+  
+})
